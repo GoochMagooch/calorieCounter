8f762cda85d8ebeff74f069f310393d2eead931d
Magooch <crvallecito@gmail.com>
2025-03-09 17:57:40 -0600

    Create functions to add foods, calculate total calories and clear foods array

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let inputFood = document.getElementById('enter-food')
 let quantity = document.getElementById('quant')
 let measurement = document.getElementById('measurement')
+let addFood = document.getElementById('add-food')
 let calculate = document.getElementById('calculate')
+let clear = document.getElementById('clear')
 let output = document.getElementById('output')
 let outputVal = output.innerText
+let storedFoods = []
 
 // Object holds foods and calories in 1oz
 let caloriesOz = {"chicken (white)": 34,
@@ -14,16 +17,43 @@ let caloriesOz = {"chicken (white)": 34,
                   "lamb": 83,
                   "salmon": 59}
 
-// calculates calories from entered food
-calculate.addEventListener('click', function() {
-    output.textContent = outputVal
+// stores foods in array to be calculated
+function storeFoods(value) {
+    storedFoods.push(value)
+}
+
+// adds food to array on button click
+addFood.addEventListener('click', function() {
     let food = inputFood.value
     let amount = quantity.value
     let measured = measurement.value
+    let argValueOz = 0
+    let argValueLb = 0
     if (!food || !amount || !measured) {
         alert("fill out all fields")
     } else {
-        measured === 'oz' ? output.textContent += ' ' + caloriesOz[food] * amount : 
-        output.textContent += ' ' + caloriesOz[food] * amount * 16
+        if (measured === 'oz') {
+            argValueOz += caloriesOz[food] * amount
+            storeFoods(argValueOz)
+        } else {
+            argValueLb += caloriesOz[food] * amount * 16
+            storeFoods(argValueLb)
+        }
     }
+})
+
+// calculates calories from entered food
+calculate.addEventListener('click', function() {
+    output.textContent = outputVal
+    let total = 0
+    for (let i = 0; i < storedFoods.length; i++) {
+        total += storedFoods[i]
+    }
+    output.textContent += ' ' + total
+})
+
+// clear calories database
+clear.addEventListener('click', function() {
+    storedFoods = []
+    alert('foods have been cleared.')
 })
