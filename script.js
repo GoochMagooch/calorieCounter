23e3abdde111c5f4bb92b9d93de3e2c4fecb8b71
Magooch <crvallecito@gmail.com>
2025-03-09 13:58:42 -0600

    Modify function to accept quantity input and fill calories object with more foods

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let output = document.getElementById('output')
-let calculate = document.getElementById('calculate')
 let inputFood = document.getElementById('enter-food')
-let caloriesOz = {"chicken": 68}
+let quantity = document.getElementById('quant')
+let measurement = document.getElementById('measurement') // on hold for now
+let calculate = document.getElementById('calculate')
+let output = document.getElementById('output')
+let outputVal = output.innerText
+
+// Object holds foods and calories in 1oz
+let caloriesOz = {"chicken": 68, 
+                  "beef": 71,
+                  "lamb": 83,
+                  "turkey": 54,
+                  "salmon": 59}
 
 calculate.addEventListener('click', function() {
+    output.textContent = outputVal
     let food = inputFood.value
-    output.textContent += caloriesOz[food]
+    let amount = quantity.value
+    if (!amount) {
+        output.textContent += ' ' + caloriesOz[food]
+    } else {
+        output.textContent += ' ' + caloriesOz[food] * amount
+    }
 })
