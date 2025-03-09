e7b99d99ea4ffc410f638c1d7075db97064346f8
Magooch <crvallecito@gmail.com>
2025-03-08 23:51:17 -0700

    Create variables and start calculate function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let calories = {"chicken": 231}
+let output = document.getElementById('output')
+let calculate = document.getElementById('calculate')
+let inputFood = document.getElementById('enter-food')
+let caloriesOz = {"chicken": 68}
+
+calculate.addEventListener('click', function() {
+    let food = inputFood.value
+    output.textContent += `${caloriesOz.food}`
+})
